bd7dc0168f15a6462a8ee47fcd2825bd4f1161fb
joonho_Lee <goodfluence@naver.com>
2021-04-02 12:51:06 +0900

    서버로부터 피드 컨텐츠 불러오기

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,48 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [list, setList] = useState([]);
+  const api = axios.create({
+    baseURL: "https://problem.comento.kr",
+    header: {
+      Accept: "application/json",
+    },
+  });
+
+  const getPostList = async () => {
+    const temp = await api.get("/api/list", {
+      params: {
+        page: 1,
+        ord: "asc",
+        category: [1],
+        limit: 10,
+      },
+    });
+    setList(temp.data.data);
+    console.log(list);
+  };
+
+  const getCategoryList = async () => {
+    const categoryList = await api.get("/api/category");
+  };
+
+  useEffect(() => {
+    getPostList();
+  }, []);
+
+  // getCategoryList();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((item, index) => (
+        <div key={index}>
+          <li> {item.id}</li>
+          <li> {item.contents}</li>
+          <li> {item.title}</li>
+        </div>
+      ))}
     </div>
   );
 }
