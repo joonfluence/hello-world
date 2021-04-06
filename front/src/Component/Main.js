9e15252d6246b3bfeebd81b969ec0d93694ca89c
joonho_Lee <goodfluence@naver.com>
2021-04-07 04:53:18 +0900

    Main Page, 초기 렌더링 에러 Issue 해결

--- front/src/Component/Main.js
+++ front/src/Component/Main.js
@@ -8,7 +8,6 @@ import Filter from "./Filter";
 import Nav from "./Nav";
 
 const Main = () => {
-  const [loading, setLoading] = useState(true);
   const [type, setType] = useState([1, 2, 3]);
   const [limit, setLimit] = useState(10);
   const [order, setOrder] = useState("asc");
@@ -21,6 +20,7 @@ const Main = () => {
   const CATEGORY_ID_3 = "coconut";
 
   const checkIsAd = (i) => i % 4 === 2;
+
   const fetchPostList = async (pageNum, order, categoryNum, limitNum) => {
     const temp = await getPostList(pageNum, order, categoryNum, limitNum);
     setList(temp);
@@ -32,16 +32,12 @@ const Main = () => {
   };
 
   useEffect(() => {
-    fetchPostList(1, order, type, limit);
-  }, [type, limit, order]);
-
-  useEffect(() => {
     fetchADList(1, limit);
   }, [limit]);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+    fetchPostList(1, order, type, limit);
+  }, [type, limit, order]);
 
   useEffect(() => {
     const scrollEvent = () => {
@@ -66,63 +62,59 @@ const Main = () => {
 
   return (
     <main>
-      {loading ? (
-        <div>로딩중...</div>
-      ) : (
-        <div>
-          <Nav />
-          <div className="main__wrapper">
-            <div className="main__login">
-              <span>로그인</span>
-            </div>
-            <div className="main__container">
-              <Filter
-                type={type}
-                setType={setType}
-                order={order}
-                setOrder={setOrder}
-              />
-              {list.map((item, i) => (
-                <>
-                  <div className="main__content--wrapper" key={item.id}>
-                    <Link to={`/` + item.id}>
-                      <div className="main__header">
-                        <div>
-                          <div className="main__header--container">
-                            <div className="main__header--category">
-                              {item.category_id === 1
-                                ? CATEGORY_ID_1
-                                : item.category_id === 2
-                                ? CATEGORY_ID_2
-                                : CATEGORY_ID_3}
-                            </div>
-                            <div className="main__header--id">{item.id}</div>
+      <div>
+        <Nav />
+        <div className="main__wrapper">
+          <div className="main__login">
+            <span>로그인</span>
+          </div>
+          <div className="main__container">
+            <Filter
+              type={type}
+              setType={setType}
+              order={order}
+              setOrder={setOrder}
+            />
+            {list.map((item, i) => (
+              <>
+                <div className="main__content--wrapper" key={item.id}>
+                  <Link to={`/` + item.id}>
+                    <div className="main__header">
+                      <div>
+                        <div className="main__header--container">
+                          <div className="main__header--category">
+                            {item.category_id === 1
+                              ? CATEGORY_ID_1
+                              : item.category_id === 2
+                              ? CATEGORY_ID_2
+                              : CATEGORY_ID_3}
                           </div>
-                          <div className="main__header--container">
-                            <div className="main__header--userID">
-                              {item.user_id}
-                            </div>
-                            <div className="main__header--date">
-                              created_at({item.created_at.substring(0, 10)})
-                            </div>
+                          <div className="main__header--id">{item.id}</div>
+                        </div>
+                        <div className="main__header--container">
+                          <div className="main__header--userID">
+                            {item.user_id}
+                          </div>
+                          <div className="main__header--date">
+                            created_at({item.created_at.substring(0, 10)})
                           </div>
                         </div>
                       </div>
-                      <div className="main__content--container">
-                        <h3 className="main__content--title">{item.title}</h3>
-                        <div className="main__content--description">
-                          {item.contents}
-                        </div>
+                    </div>
+                    <div className="main__content--container">
+                      <h3 className="main__content--title">{item.title}</h3>
+                      <div className="main__content--description">
+                        {item.contents}
                       </div>
-                    </Link>
-                  </div>
-                  {checkIsAd(i) ? <Ad item={adList[(i - 2) / 4]} /> : null}
-                </>
-              ))}
-            </div>
+                    </div>
+                  </Link>
+                </div>
+                {checkIsAd(i) ? <Ad item={adList[(i - 2) / 4]} /> : null}
+              </>
+            ))}
           </div>
         </div>
-      )}
+      </div>
     </main>
   );
 };
